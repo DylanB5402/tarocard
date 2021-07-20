8691bfa38869757f4708a7a1580e468cd24e7fd5
rrmctapia <rrmc.tapia@yahoo.com>
2021-07-20 15:36:20 -0700

    update forms for server/begin implement of addcard

--- app.js
+++ app.js
@@ -11,3 +11,17 @@ function signUp(){
         document.getElementById('login-button').innerHTML = " Log in";
     }
 }
+
+
+// Fake login for now??
+
+function validate(){
+    console.log("Button was pressed!");
+    if(document.getElementById('userEmail').value === "user1" && document.getElementById("userPassword").value === "password1"){
+        console.log("User has logged in!")
+    } else {
+        console.log("Bred how did you mess that up");
+    }
+
+    console.log(location.href)
+}
