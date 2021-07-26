de566c4d5f0693a94fd55cb8150549217d8a64da
Dylan Barva <d.barva5402@gmail.com>
2021-07-26 13:27:32 -0700

    standardize folder names for frontend

--- static/homepage/homepage.js
+++ static/homepage/homepage.js
@@ -1,65 +1,63 @@
-function openCardCreate(){
-    document.getElementById('id01').style.display='block';
-    document.getElementById('add-card-btn').style.display='none';
-    if( document.getElementById('id01').classList.contains('dezoom')){
-        document.getElementById('id01').classList.remove('dezoom');
-    }
-    document.getElementById('id01').classList.add('animate');
+function openCardCreate () {
+  document.getElementById('id01').style.display = 'block'
+  document.getElementById('add-card-btn').style.display = 'none'
+  if (document.getElementById('id01').classList.contains('dezoom')) {
+    document.getElementById('id01').classList.remove('dezoom')
+  }
+  document.getElementById('id01').classList.add('animate')
 }
 
-function closeCardCreate(){
-    document.getElementById('id01').classList.remove('animate');
-    document.getElementById('id01').classList.add('dezoom');
-    setTimeout(function(){
-        console.log("We did it bitches");
-        document.getElementById('id01').style.display='none';
-
-    },500);
-    document.getElementById('add-card-btn').style.display='block';
+function closeCardCreate () {
+  document.getElementById('id01').classList.remove('animate')
+  document.getElementById('id01').classList.add('dezoom')
+  setTimeout(function () {
+    console.log('We did it bitches')
+    document.getElementById('id01').style.display = 'none'
+  }, 500)
+  document.getElementById('add-card-btn').style.display = 'block'
 }
-/* 
-* Request from server the cards information 
+/*
+* Request from server the cards information
 * Then add that information into the cards
 * After insertion of information, insert card into div "card-container"
 */
 
-let cardDiv = document.getElementById("cardContainer");
+const cardDiv = document.getElementById('cardContainer')
 
-function createCard(establishment, drink, description){
-    /* first step is to get the template*/
-    let template = document.getElementById('card-template');
-    let tcontent = template.content.querySelector('div');
-    let divCopy = document.importNode(tcontent,true);
-    /* Adds the class 'card-template' so cards will look like actual template'*/
-    divCopy.classList.add('card-template');
+function createCard (establishment, drink, description) {
+  /* first step is to get the template */
+  const template = document.getElementById('card-template')
+  const tcontent = template.content.querySelector('div')
+  const divCopy = document.importNode(tcontent, true)
+  /* Adds the class 'card-template' so cards will look like actual template' */
+  divCopy.classList.add('card-template')
 
-    /* Changes fields */
+  /* Changes fields */
 
-     divCopy.querySelector("#establishment").innerHTML = establishment; //establishment
-     divCopy.querySelector("#drink").innerHTML = drink; //drinks
-     divCopy.querySelector("#description").innerHTML = description; //description
-     divCopy.querySelector('div').querySelector('img').setAttribute("src","https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg");
+  divCopy.querySelector('#establishment').innerHTML = establishment // establishment
+  divCopy.querySelector('#drink').innerHTML = drink // drinks
+  divCopy.querySelector('#description').innerHTML = description // description
+  divCopy.querySelector('div').querySelector('img').setAttribute('src', 'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg')
 
-    /*Adds the card to the card container div */
-    cardDiv.appendChild(divCopy);
+  /* Adds the card to the card container div */
+  cardDiv.appendChild(divCopy)
 }
 
+/* Accessing server and putting information into cards // taken from Johnothan's friendpage */
+const request = new XMLHttpRequest()
+request.open('GET', 'https://my-json-server.typicode.com/shadydrako/cardData/db', true)
+request.responseType = 'json'
+request.send()
 
-/*Accessing server and putting information into cards // taken from Johnothan's friendpage*/
-let request = new XMLHttpRequest();
-request.open('GET', 'https://my-json-server.typicode.com/shadydrako/cardData/db', true);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    const cards = request.response.cards;
-    for(let i = 0; i < cards.length; i++) {
-        let x =  cards[i]["Name of Establishment"];
-        let y = cards[i]["Name of Order"];
-       let z =cards[i]["Description"];
-       createCard2(x,y,z,"../assets/pfp-placeholder.png");
-       console.log(i);
-    }
+request.onload = function () {
+  const cards = request.response.cards
+  for (let i = 0; i < cards.length; i++) {
+    const x = cards[i]['Name of Establishment']
+    const y = cards[i]['Name of Order']
+    const z = cards[i].Description
+    createCard2(x, y, z, '../assets/pfp-placeholder.png')
+    console.log(i)
+  }
 }
 
 /* Function created to help with creating cards for other pages
@@ -67,44 +65,43 @@ request.onload = function() {
 *  need to add the stylesheet for cards if they plan to have cards
 */
 
+function createCard2 (establishment, drink, description, image) {
+  const container = document.createElement('div') // This creates div element
+  container.classList.add('card-template')
+  /* Create establishment element */
+  const estab = document.createElement('h1')
+  estab.classList.add('fonts')
+  estab.innerHTML = establishment
+  /* Create drink element */
 
-function createCard2(establishment, drink, description, image){
-    const container = document.createElement("div"); //This creates div element
-    container.classList.add("card-template");
-    /* Create establishment element */
-    let estab = document.createElement("h1");
-    estab.classList.add("fonts");
-    estab.innerHTML = establishment;
-    /* Create drink element */
+  const d = document.createElement('h2')
+  d.classList.add('fonts')
+  d.innerHTML = drink
 
-    let d = document.createElement("h2");
-    d.classList.add("fonts");
-    d.innerHTML = drink;
+  /* Create description element */
 
-    /* Create description element */
+  const desc = document.createElement('h2')
+  desc.classList.add('fonts')
+  desc.style = 'font-weight: 100'
+  desc.innerHTML = description
 
-    let desc = document.createElement("h2");
-    desc.classList.add("fonts");
-    desc.style = "font-weight: 100";
-    desc.innerHTML = description;
+  /* Create tag container */
 
-    /*Create tag container*/
+  const tagContainer = document.createElement('div')
+  tagContainer.classList.add('tag-container')
 
-    let tagContainer = document.createElement("div");
-    tagContainer.classList.add("tag-container");
+  /* profile picture */
+  const pfp = document.createElement('img')
+  pfp.classList.add('pfp-pic')
+  pfp.setAttribute('src', image)
 
-    /* profile picture */
-    let pfp = document.createElement("img");
-    pfp.classList.add("pfp-pic");
-    pfp.setAttribute("src", image);
-    
-    tagContainer.appendChild(pfp);
+  tagContainer.appendChild(pfp)
 
-    container.appendChild(estab);
-    container.appendChild(d);
-    container.appendChild(desc);
-    container.appendChild(tagContainer);
-    document.getElementById('cardContainer').appendChild(container);
+  container.appendChild(estab)
+  container.appendChild(d)
+  container.appendChild(desc)
+  container.appendChild(tagContainer)
+  document.getElementById('cardContainer').appendChild(container)
 }
 
-module.export = { createCard2 };
+module.export = { createCard2 }
