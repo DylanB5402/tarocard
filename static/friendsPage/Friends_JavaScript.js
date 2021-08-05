1e9fe2f0a0a345b756547192e276302826490d62
Dylan Barva <d.barva5402@gmail.com>
2021-08-05 09:51:02 -0700

    search friends works

--- static/friendsPage/Friends_JavaScript.js
+++ static/friendsPage/Friends_JavaScript.js
@@ -2,41 +2,30 @@
 const body = document.querySelector('body')
 
 const request = new XMLHttpRequest()
-// request.open('GET', 'https://my-json-server.typicode.com/VitalKilla/userData/db', true)
-// send request to '/friends/current/
 request.open('GET', '/friends/current', true)
 request.responseType = 'json'
 request.send()
 
 request.onload = function () {
   const users = request.response
-  // console.log(users)
   populateFriends(users)
 }
 
 // function is used to fill page with list of friends from a json file.
 function populateFriends (obj) {
-  console.log(obj)
   let template, divAll, div, divCopy, divHeader, divHeaderCopy, currentLetter, char
   divAll = document.createElement('div')
   divAll.id = 'friendsAll'
   template = document.getElementById('friendsTemp')
   div = template.content.querySelector('.friendElement')
   divHeader = template.content.querySelector('#headerElement')
-  // const users = obj.users;
-  // used to test different list of users
-  // const users = obj.usersOther
   const users = obj.users
-  // currentLetter = users[0]['display name'].charAt(0)
-  // currentLetter = users[0]['display name'].charAt(0)
   currentLetter = users[0]['display name'].charAt(0).toUpperCase()
   divHeaderCopy = document.importNode(divHeader, true)
   divHeaderCopy.querySelector('h1').textContent = currentLetter
   divAll.appendChild(divHeaderCopy)
   for (let i = 0; i < users.length; i++) {
     divCopy = document.importNode(div, true)
-    // char refers to first letter of current users display name
-    // char = users[i]['display name'].charAt(0)
     char =  users[i]['display name'].charAt(0).toUpperCase()
     if (currentLetter !== char) {
       currentLetter = char
@@ -59,15 +48,18 @@ function searchFriends (obj) {
   template = document.getElementById('friendsTemp')
   div = template.content.querySelector('.friendElement')
   divHeader = template.content.querySelector('#headerElement')
-  const users = obj.searchUsers
-  currentLetter = users[0]['display name'].charAt(0)
+  const users = obj.users
+  if (users[0] === undefined) {
+    return undefined;
+  }
+  currentLetter = users[0]['display name'].charAt(0).toUpperCase()
   divHeaderCopy = document.importNode(divHeader, true)
   divHeaderCopy.querySelector('h1').textContent = currentLetter
   divAll.appendChild(divHeaderCopy)
   for (let i = 0; i < users.length; i++) {
     divCopy = document.importNode(div, true)
     // char refers to first letter of current users display name
-    char = users[i]['display name'].charAt(0)
+    char = users[i]['display name'].charAt(0).toUpperCase()
     if (currentLetter !== char) {
       currentLetter = char
       divHeaderCopy = document.importNode(divHeader, true)
