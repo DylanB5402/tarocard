417394216e1913c040d00e968cea82f081cf4dea
rrmctapia <rrmc.tapia@yahoo.com>
2021-08-12 13:55:20 -0700

    group View changed to a div

--- static/homepage/groupOrder.js
+++ static/homepage/groupOrder.js
@@ -1,25 +1,4 @@
-let request = new XMLHttpRequest();
-request.open('GET', '/drinks', true);
-request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    const cards = request.response.cards;
-    for(let i = 0; i < cards.length; i++) {
-        let x =  cards[i]["Name of Establishment"];
-        let y = cards[i]["Name of Order"];
-       let z =cards[i]["Description"];
-       createCard(x,y,z,"../assets/pfp-placeholder.png");
-       console.log(i);
-    }
-}
-
-/* Function created to help with creating cards for other pages
-*  Styling needed for each one, thus the class added to them so everyone might
-*  need to add the stylesheet for cards if they plan to have cards
-*/
-/* add the card id */
- function createCard(establishment, drink, description, image){
+function createCard(establishment, drink, description, image, drinkID){
     const container = document.createElement("div"); //This creates div element
     container.classList.add("card-template");
     /* Create establishment element */
@@ -48,41 +27,57 @@ request.onload = function() {
     pfp.classList.add("pfp-pic");
     pfp.setAttribute("src", image);
 
+    /* Options Button */
 
-    /* Trash Icon Button */
-    let deleteBtn  = document.createElement('img');
+    /* Delete Button */
+    let deleteBtn = document.createElement("img");
     deleteBtn.src="../assets/trash-icon.png";
-    deleteBtn.setAttribute('width','30px')
-    deleteBtn.style.display = "inline-block";
-    deleteBtn.style.position = "absolute";
-    deleteBtn.style.right = "0px";
-    deleteBtn.style.margin = "10px";
+    deleteBtn.classList.add("trash-card");
 
     deleteBtn.onclick = function(){
-        console.log("whats good bro");
         container.style.display = "none";
-        /*Delete based on the cardID */
-
-        const url = "https://my-json-server.typicode.com/shadydrako/cardData/cards" /* +"/" + userID */
-
-        const deleteData = async () => {
-            const response = await fetch('url',{
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: null
-            });
-        }
+        /* Sending a delete requestGroupContent with this button */
+        fetch( 'https://localhost:3000/drinks/' + drinkID ,{
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({id: drinkID}) //sending drinkID 
+        }); 
     }
-
-    
     tagContainer.appendChild(pfp);
-    container.appendChild(deleteBtn);
 
     container.appendChild(estab);
     container.appendChild(d);
     container.appendChild(desc);
     container.appendChild(tagContainer);
-    document.getElementById('cardContainer').appendChild(container);
+    container.appendChild(deleteBtn);
+    document.getElementById('groupContainer').appendChild(container);
+}
+
+    let requestGroupContent = new XMLHttpRequest();
+    requestGroupContent.open('GET', 'https://my-json-server.typicode.com/shadydrako/cardData/db', true);
+    requestGroupContent.responseType = 'json';
+    requestGroupContent.send();
+
+    requestGroupContent.onload = function() {
+        const cards = requestGroupContent.response.cards;
+        for(let i = 0; i < cards.length; i++) {
+            let x =  cards[i]["Name of Establishment"];
+            let y = cards[i]["Name of Order"];
+            let z =cards[i]["Description"];
+            let drinkId = cards[i]["id"]
+            /*
+            console.log(drinkCard)
+            const drinkEst = cards[drinkCard]['establishment']
+            const drinkName = cards[drinkCard]['name']
+            const drinkDesc = cards[drinkCard]['desc']
+            const drinkId = cards[drinkCard]['id']
+            createCard2(drinkEst, drinkName, drinkDesc, '../assets/pfp-placeholder.png', drinkId)
+            */
+        createCard(x,y,z,"../assets/pfp-placeholder.png",drinkId);
+        console.log(i);
+        }
+    }
+
+document.getElementById("closeGroupView").onclick = function (){
+    document.getElementById("groupView").style.display="none";
 }
