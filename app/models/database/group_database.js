5967c447fa692be5e671c478a04a9b9b35a0cf30
Alex Nguyen <atn012@ucsd.edu>
2021-08-12 17:35:36 -0700

    group id not null anymore haha :')

--- app/models/database/group_database.js
+++ app/models/database/group_database.js
@@ -122,15 +122,42 @@ class GroupDatabase {
     if (!userDB.getUserByUID(uid)) {
       return null
     } else {
-      const stmt = this.db.prepare('INSERT INTO groups (group_id, uid, group_name, friend_uid, friends_drink_id)' +
-              'VALUES ((SELECT max(group_id) + 1 FROM groups), ?, ?, ?, ?)')
-      const query = stmt.run(uid, groupName, friendUID, friendsDrinkID)
 
-      if (query.changes === 1) {
-        return query.lastInsertRowid
+      const emptyData = this.db.prepare('select count(*) count from (select 1) where exists (select * from groups);')
+      const emptyQuery = emptyData.get()
+      const empty = emptyQuery.count
+
+      console.log("This is empty: " + empty)
+
+      // Check for empty table
+      if (empty === 0) {
+        const stmt = this.db.prepare('INSERT INTO groups (group_id, uid, group_name, friend_uid, friends_drink_id)' +
+                'VALUES (?, ?, ?, ?, ?)')
+        const query = stmt.run(0, uid, groupName, friendUID, friendsDrinkID)
+
+        if (query.changes === 1) {
+          return query.lastInsertRowid
+        } else {
+          return null
+        }
       } else {
-        return null
+
+        const maxGroupIdStmt = this.db.prepare('SELECT (max(group_id) + 1) max FROM groups')
+        const maxGroupIdQuery = maxGroupIdStmt.get()
+        const maxGroupId = maxGroupIdQuery.max
+        console.log('This is the max group id: ' + maxGroupId)
+
+        const stmt = this.db.prepare('INSERT INTO groups (group_id, uid, group_name, friend_uid, friends_drink_id)' +
+                'VALUES ((SELECT max(group_id) + 1 FROM groups), ?, ?, ?, ?)')
+        const query = stmt.run(uid, groupName, friendUID, friendsDrinkID)
+
+        if (query.changes === 1) {
+          return query.lastInsertRowid
+        } else {
+          return null
+        }
       }
+     
     }
   }
 
