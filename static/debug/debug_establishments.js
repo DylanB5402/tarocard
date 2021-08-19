c1459f471e74f3b9e7df7c2ab36e00f15266b18a
Potor <liupotor4@gmail.com>
2021-08-19 13:13:29 -0700

    forgot to save changes

--- static/debug/debug_establishments.js
+++ static/debug/debug_establishments.js
@@ -47,7 +47,7 @@ let getEstabishments = () => {
   });
 }
 
-function autocomplete(inp, arr) {
+function autocomplete(inp, arr, maxDisplay) {
   /*the autocomplete function takes two arguments,
   the text field element and an array of possible autocompleted values:*/
   var currentFocus;
@@ -65,10 +65,12 @@ function autocomplete(inp, arr) {
     a.setAttribute("class", "autocomplete-items");
     /*append the DIV element as a child of the autocomplete container:*/
     this.parentNode.appendChild(a);
+
+    let displayed = 0
     /*for each item in the array...*/
     for (i = 0; i < arr.length; i++) {
       /*check if the item starts with the same letters as the text field value:*/
-      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase() && displayed < maxDisplay) {
         /*create a DIV element for each matching element:*/
         b = document.createElement("DIV");
         /*make the matching letters bold:*/
@@ -77,14 +79,15 @@ function autocomplete(inp, arr) {
         /*insert a input field that will hold the current array item's value:*/
         b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
         /*execute a function when someone clicks on the item value (DIV element):*/
-            b.addEventListener("click", function(e) {
-            /*insert the value for the autocomplete text field:*/
-            inp.value = this.getElementsByTagName("input")[0].value;
-            /*close the list of autocompleted values,
-            (or any other open lists of autocompleted values:*/
-            closeAllLists();
+        b.addEventListener("click", function(e) {
+          /*insert the value for the autocomplete text field:*/
+          inp.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+          (or any other open lists of autocompleted values:*/
+          closeAllLists();
         });
         a.appendChild(b);
+        displayed++;
       }
     }
   });
@@ -139,7 +142,7 @@ function autocomplete(inp, arr) {
     var x = document.getElementsByClassName("autocomplete-items");
     for (var i = 0; i < x.length; i++) {
       if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
+        x[i].parentNode.removeChild(x[i]);
     }
   }
 }
@@ -153,5 +156,7 @@ document.addEventListener("click", function (e) {
 fetch(`/establishments/unique`)
   .then((response) => response.json())
   .then((data) => {
-    autocomplete(document.getElementById('search'), data)
+    // Maximum elements to show in autocomplete
+    let maxDisplay = 10
+    autocomplete(document.getElementById('search'), data, maxDisplay)
 });
