dc46502b1e21ef14ca7c30f6a422046337bd4855
FIRST_NAME LAST_NAME <jos012@ucsd.edu>
2021-08-19 18:36:04 -0700

    worked on friend requests (not complete)

--- static/friendsPage/friendsJava.js
+++ static/friendsPage/friendsJava.js
@@ -37,7 +37,7 @@ function populateFriends (obj) {
     divCopy.querySelector('img', true).src = users[i]['image url']
     divCopy.querySelector('h2', true).textContent = users[i]['display name']
     divCopy.querySelector('h3', true).textContent = users[i].username;
-    let links = divCopy.querySelectorAll(".userProLink");
+    let links = divCopy.querySelectorAll(".ActivityLinks");
     links.forEach(element => {
         element.href = "/profile/" + users[i]["id"];
     });
